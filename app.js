c3be4fa81f7476d171db2513858a0f4060325329
Geunmo Kim <kimgeunmo@gmail.com>
2022-02-14 22:07:07 +0100

    2D Context

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
-const stopPainting = (event) => {
-  paiting = false;
+const stopPainting = () => {
+  painting = false;
+};
+
+const startPainting = () => {
+  painting = true;
 };
 
 const onMouseMove = (event) => {
   const x = event.offsetX;
   const y = event.offsetY;
-};
-
-const onMouseDown = (event) => {
-  painting = true;
-};
-
-const onMouseUp = (event) => {
-  stopPainting();
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
